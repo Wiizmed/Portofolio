8bc467a38f0535d304adabac6d59e0d84ce3987a
Wiizmed <louizmohamed44@gmail.com>
2024-09-17 11:55:04 +0100

    final

--- my-portfolio/src/Components/About.jsx
+++ my-portfolio/src/Components/About.jsx
@@ -5,8 +5,8 @@ const About = () => (
     <div className="container mx-auto px-4">
       <h2 className="text-3xl font-bold text-center mb-8">About Me</h2>
       <div className="text-lg text-gray-700 space-y-4">
-  <p>I graduated from <span className="font-semibold">RBK Coding Bootcamp</span> with a focus on <span className="font-semibold">Full Stack Web Development</span>. Over the past <span className="font-semibold">2 years</span>, I’ve gained hands-on experience in building dynamic web applications, from concept to deployment.</p>
-  <p>Proficient in a wide range of technologies including <span className="font-semibold">JavaScript, React, Node.js, Express, MongoDB</span>, and other modern development tools. My expertise spans both front-end and back-end development, allowing me to contribute to all layers of the development stack.</p>
+  <p>I graduated from <span className="font-semibold">RBK Coding Bootcamp</span> with a focus on <span className="font-semibold">Full Stack Web Development</span>. Over the past <span className="font-semibold"> 6 Monthes</span>, I’ve gained hands-on experience in building dynamic web applications, from concept to deployment.</p>
+  <p> My expertise spans both front-end and back-end development, allowing me to contribute to all layers of the development stack.</p>
   <p>Throughout my journey as a junior developer, I have successfully built and deployed multiple full-stack applications, demonstrating my ability to work in agile environments, manage databases, and implement responsive designs. I am particularly passionate about creating intuitive user experiences, optimizing performance, and solving complex technical challenges.</p>
   <p>Driven by curiosity and a passion for continuous learning, I am eager to expand my knowledge in emerging technologies and grow within a collaborative, fast-paced team. I’m looking forward to bringing my technical skills and problem-solving mindset to real-world projects in the field of web and Mobile development.</p>
 </div>
